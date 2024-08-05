b0fb3ce5d637b876e551756bb1afc686946dc421
Peter Gallwas [peg0252] (they/them) <peg0252@arastudent.ac.nz>
2024-08-06 11:53:58 +1200

    add the thingy i need to get working:

--- fnamelnamec.py
+++ fnamelnamec.py
@@ -10,5 +10,8 @@ user_message = input(f"{user_first_name}, please enter a message: ").strip()
 user_position_1 = int(input("Please enter the start position: "))
 user_position_2 = int(input("Please enter the end position: "))
 
+string_at_position = user_message[user_position_1:user_position_2 + 1]
+
+
 print(f"Report for {user_first_name} {user_last_name}'s sentence of {len(user_message)} characters.")
-print(f"The characters '{user_message}' can be found in position {user_position_1} to position {user_position_2} inclusively.")
+print(f"The characters '{string_at_position}' can be found in position {user_position_1} to position {user_position_2} inclusively.")
